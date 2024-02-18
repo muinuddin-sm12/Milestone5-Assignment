c4b7db8b7090eb0aa0383c3058a2975ebb3c1a22
Muin-Uddin-Ahmad <muinuddin.sm12@gamil.com>
2024-02-19 02:02:26 +0600

    Append seat details added

--- script.js
+++ script.js
@@ -14,8 +14,85 @@ for(const seat of seatButton){
             }
         }
         const selectCount = document.getElementById('seat-count');
-        selectCount.innerText = selectSeat
+        selectCount.innerText = selectSeat;
         const availableSeat = document.getElementById('available-seat');
-        availableSeat.innerText = (40 - selectSeat);        
+        availableSeat.innerText = (8 - selectSeat);  
     })
 }
+
+let totalPrice = 0;
+let totalSelect = 0;
+const button = document.getElementsByClassName('seat-button');
+for(const seat of seatButton){
+    seat.addEventListener('click', function(){
+        totalSelect++;
+        if(totalSelect>4){
+            seat.removeEventListener('click')
+        }else{
+            const seatName = seat.innerText;
+        const seatDetails = document.getElementById('seat-details-container');
+        const li = document.createElement('li');
+        li.classList.add('seat-details');
+        const P1 = document.createElement('p')
+        P1.innerText = seatName;
+        li.appendChild(P1);
+        const P2 = document.createElement('p');
+        P2.innerText = 'Economy';
+        li.appendChild(P2);
+        const fare = document.createElement('p');
+        fare.innerText = 550;
+        li.appendChild(fare);
+        seatDetails.appendChild(li);
+
+        const price = parseInt(fare.innerText);
+        console.log(price)
+        totalPrice = totalPrice+price;
+
+        const totalPriceContainer = document.getElementById('total-price');
+        totalPriceContainer.innerText = totalPrice;
+
+        const grandTotalContainer = document.getElementById('grand-total-price');
+        grandTotalContainer.innerText = totalPrice;
+
+            document.getElementById('apply-btn').addEventListener('click', function(){
+                const inputElement = document.getElementById('input-element');
+                if(inputElement.value === 'NEW15'){
+                    const grandTotal = (totalPrice*15)/100;
+                    grandTotalContainer.innerText = totalPrice-grandTotal;
+                    inputElement.value = '';
+                    const inputField =document.getElementById('input-field');
+                    inputField.style.display = 'none';
+                }
+                else if(inputElement.value === 'Couple 20'){
+                    const grandTotal = (totalPrice*20)/100;
+                    grandTotalContainer.innerText = totalPrice - grandTotal;
+                    inputElement.value = '';
+                    const inputField =document.getElementById('input-field');
+                    inputField.style.display = 'none';
+                }
+                else{
+                    // alert('Invalid Coupon Code');
+                    inputElement.value = '';
+                }
+                
+            })
+
+        }
+
+    })
+}
+
+
+const nextButton = document.getElementById('next-btn');
+nextButton.addEventListener('click', function(){
+    const head = document.getElementById('head')
+    head.style.display = 'none';
+    const main = document.getElementById('main')
+    main.style.display = 'none';
+    const foot = document.getElementById('foot')
+    foot.style.display = 'none';
+
+    const modal = document.getElementById('modal');
+    modal.classList.remove('hidden');
+})
+
